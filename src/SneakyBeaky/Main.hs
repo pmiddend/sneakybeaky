6dd7e763d0ca19c150eac9c81491a2b36655c47a
Philipp Middendorf <pmidden@secure.mailbox.org>
2014-12-29 19:37:36 +0100

    Add skeleton for obstacle tiles, add render architecture

--- src/SneakyBeaky/Main.hs
+++ src/SneakyBeaky/Main.hs
@@ -5,11 +5,25 @@ import System.Console.ANSI
 import System.IO
 import Control.Monad.IO.Class(MonadIO,liftIO)
 import Control.Exception.Base(bracket_)
+import System.Random
+import Control.Monad.Random
 
 type Coord = (Int, Int)
 
+data Tile = Tile {
+    tPosition :: Coord
+  , tCharacter :: Char
+  , tSgr :: [SGR]
+  }
+
+data ObstacleTile = ObstacleTile {
+    oTile :: Tile
+  , oSolid :: Bool
+  }
+
 data World = World {
-  wHero :: Coord
+    wHero :: Coord
+  , wObstacles :: [ObstacleTile]
 }
 
 data Input = Up
@@ -20,28 +34,43 @@ data Input = Up
            deriving (Eq)
 
 gameTitle :: String
-gameTitle = "Thieflike"
+gameTitle = "sneakybeaky"
+
+initialWorld :: World
+initialWorld = World (0,0) []
+
+generateObstacles :: RandomGen g => Rand g [ObstacleTile]
+generateObstacles = return []
 
 main :: IO ()
 main = bracket_ (hSetEcho stdin False >> hSetBuffering stdin  NoBuffering >> hSetBuffering stdout NoBuffering >> hideCursor) (showCursor >> hSetEcho stdin True) $ do
+  obstacles <- evalRandIO generateObstacles
   setTitle gameTitle
-  gameLoop $ World (0, 0)
+  gameLoop (World (0,0) obstacles)
+
+renderWorld :: World -> [Tile]
+renderWorld w = [renderHero (wHero w)]
+
+renderHero :: Coord -> Tile
+renderHero c = Tile { tCharacter = '@', tSgr = [SetConsoleIntensity BoldIntensity, SetColor Foreground Vivid Blue ], tPosition = c }
 
 gameLoop :: MonadIO m => World -> m ()
-gameLoop world@(World hero) = do
+gameLoop w = do
   liftIO clearScreen
-  drawHero hero
+  mapM_ drawTile (renderWorld w)
   input <- getInput
   case input of
     Exit -> return ()
-    _    -> handleDir world input
+    _    -> handleDir w input
 
-drawHero :: MonadIO m => (Int,Int) -> m ()
-drawHero (heroX, heroY) = do
-  liftIO $ setCursorPosition heroY heroX
-  liftIO $ setSGR [ SetConsoleIntensity BoldIntensity
-                  , SetColor Foreground Vivid Blue ]
-  liftIO $ putStr "@"
+drawTile :: MonadIO m => Tile -> m ()
+drawTile t = do
+  liftIO $ setCursorPosition (snd (tPosition t)) (fst (tPosition t))
+  liftIO $ setSGR (tSgr t)
+  liftIO $ putStr [tCharacter t]
+
+drawHero :: MonadIO m => Coord -> m ()
+drawHero c = drawTile $ Tile { tCharacter = '@', tSgr = [SetConsoleIntensity BoldIntensity, SetColor Foreground Vivid Blue ], tPosition = c}
 
 -- receive a character and return our Input data structure,
 -- recursing on invalid input
@@ -59,8 +88,9 @@ getInput = do
 -- given a world and a direction, 'adjust' the hero's position, and loop
 -- with our updated hero
 handleDir :: MonadIO m => World -> Input -> m ()
-handleDir w@(World (heroX, heroY)) input = gameLoop $ w { wHero = newCoord }
-  where newCoord = case input of
+handleDir w input = gameLoop $ w { wHero = newCoord }
+  where (heroX,heroY) = wHero w
+        newCoord = case input of
                     Up    -> (heroX, heroY - 1)
                     Down  -> (heroX, heroY + 1)
                     Left  -> (heroX - 1, heroY)
