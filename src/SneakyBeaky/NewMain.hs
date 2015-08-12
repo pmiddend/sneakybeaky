9732ee931a2989c94c7ef27f20c11a0b50d76c4f
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-08-12 19:24:06 +0200

    half-assed shit because fml today :P

--- src/SneakyBeaky/NewMain.hs
+++ src/SneakyBeaky/NewMain.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import           Linear.V2
+import Control.Lens(makeLenses,(^.))
+import System.Control.SneakyTerm.Tile
 
 type Point = V2 Int
+
+data Translucency = Translucent
+                  | Opaque
 
 data Player = Player {
     _playerPosition    :: Point
@@ -10,6 +16,8 @@ data Player = Player {
   , _playerKillArrows  :: Int
   , _playerKeys        :: [Key]
   }
+
+$(makeLenses ''Player)
 
 data EnemyState = EnemyRelaxed
                 | EnemyAlert
@@ -27,7 +35,7 @@ data Solid = Solid {
     _solidCharacter   :: Char
   , _solidPosition    :: Point
   , _solidMoveable    :: Bool
-  , _solidTranslucent :: Bool
+  , _solidTranslucency :: Translucency
   , _solidSeen        :: Bool
   }
 
@@ -70,3 +78,60 @@ data Game = Game {
   , _gameKillArrows  :: [(Point,KillArrow)]
   }
 
+{-
+Notes on rendering
+
+  - The player can see in all directions at once
+  - The player can see...
+    - everything lit by a lamp
+    - everything with the seen flag
+    - himself
+    - everything in its sight radius
+
+  - Lamp visibility determination...
+    - take all points in the circle boundary around the lamp
+    - cast a ray from the lamp to the point, mark every point on the line as lit until a non-translucent solid is encountered.
+
+  - During rendering, lit tiles are marked with a dot (.), unless they're occupied by something else already
+  - Player visibility determination...
+    - for all lit tiles, cast a ray from the player to the tile, show only if an intersection occurs
+    - for all objects inside the player near sight radius, do the lamp visibility determination test, render those, too
+
+  - Lamps do not move and certain solids don't, too, so some of the visibility work can be done statically?
+  - What data structures are returned?
+-}
+
+-- | Return all points lit by the lamp
+lampLitTiles :: Lamp                 -- ^ Lamp to test 
+             -> (PointInt -> Translucency)   -- ^ Tile to translucency
+             -> [PointInt]           -- ^ All lit points
+lampLitTiles = error "lampLitTiles not implemented"
+
+visibleTiles :: Game -> [Tile]
+visibleTiles = error "visibleTiles not implemented"
+
+initialGame = Game{
+  _gamePlayer = Player{
+       _playerPosition = V2 10 10
+     , _playerWaterArrows = 0
+     , _playerKillArrows = 0
+     , _playerKeys = []
+    },
+  _gameEnemies = [],
+  _gameSolids = [],
+  _gameExit = Exit{_exitPosition = V2 1 1},
+  _
+              
+  }
+
+playerTile :: Player -> Tile
+playerTile p = Tile{
+      _tilePosition = p ^. playerPosition
+    , _tileCharacter = '@'
+    , _tileColor = ColorPair 
+  }
+
+gameToTiles :: Game -> [Tile]
+gameToTiles g = 
+
+main = 
